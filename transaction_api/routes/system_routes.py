18625f242382bbb13f124225e738e723790d288f
Nzati stephane <stephnzati@gmail.com>
2026-02-13 00:24:01 +0100

    Add: squelette du router

---
+++ transaction_api/routes/system_routes.py
@@ -0,0 +1,9 @@
+"""System API routes."""
+
+from fastapi import APIRouter
+
+from transaction_api.logging_config import get_logger
+
+logger = get_logger(__name__)
+
+router: APIRouter = APIRouter(prefix="/api/system", tags=["system"])
