ea10a881c04db3a681791017f174d6cae4b380d7
Nzati stephane <stephnzati@gmail.com>
2026-02-13 01:13:23 +0100

    feat: structure de base du modele pydantic

---
+++ transaction_api/models.py
@@ -0,0 +1,9 @@
+"""Pydantic models for the Transaction API."""
+
+from datetime import date as date_type
+from datetime import datetime
+from typing import Generic, List, Optional, TypeVar
+
+from pydantic import BaseModel, Field
+
+T = TypeVar("T")
