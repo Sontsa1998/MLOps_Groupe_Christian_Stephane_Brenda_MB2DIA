9f5074f65920cddce2110a6276e32cb0a2c5abd9
Nzati stephane <stephnzati@gmail.com>
2026-02-13 11:27:37 +0100

    Add: squelette du logging_config

---
+++ transaction_api/logging_config.py
@@ -0,0 +1,7 @@
+"""Logging configuration for the Transaction API."""
+
+import logging
+import logging.config
+from typing import Dict, Any
+
+from transaction_api.config import LOG_LEVEL, LOG_FORMAT
