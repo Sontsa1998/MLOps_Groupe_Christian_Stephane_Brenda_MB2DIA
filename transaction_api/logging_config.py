697f6d6f3cf2e9af80e8b9f6cbd1f3a8561bf1fb
Nzati stephane <stephnzati@gmail.com>
2026-02-13 11:32:14 +0100

    Add: creation de la fonction setup_logging

--- transaction_api/logging_config.py
+++ transaction_api/logging_config.py
@@ -5,3 +5,37 @@ import logging.config
 from typing import Dict, Any
 
 from transaction_api.config import LOG_LEVEL, LOG_FORMAT
+
+
+def setup_logging() -> None:
+    """Set up logging configuration."""
+    logging_config: Dict[str, Any] = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "standard": {
+                "format": LOG_FORMAT,
+            },
+        },
+        "handlers": {
+            "default": {
+                "level": LOG_LEVEL,
+                "class": "logging.StreamHandler",
+                "formatter": "standard",
+            },
+            "file": {
+                "level": LOG_LEVEL,
+                "class": "logging.FileHandler",
+                "filename": "transaction_api.log",
+                "formatter": "standard",
+            },
+        },
+        "loggers": {
+            "": {
+                "handlers": ["default", "file"],
+                "level": LOG_LEVEL,
+                "propagate": True,
+            },
+        },
+    }
+    logging.config.dictConfig(logging_config)
