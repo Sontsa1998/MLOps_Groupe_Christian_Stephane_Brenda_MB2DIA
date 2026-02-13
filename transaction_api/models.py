adf8f10e1bbb9af2d71ea9a6065566eeeb41c101
Nzati stephane <stephnzati@gmail.com>
2026-02-13 01:17:57 +0100

    Add: creation de la classe paginationMetadata

--- transaction_api/models.py
+++ transaction_api/models.py
@@ -45,3 +45,13 @@ class Transaction(BaseModel):
                 "errors": None,
             }
         }
+
+
+ class PaginationMetadata(BaseModel):
+    """Pagination metadata."""
+
+    page: int = Field(..., description="Current page number")
+    limit: int = Field(..., description="Items per page")
+    total_count: int = Field(..., description="Total items")
+    total_pages: int = Field(..., description="Total pages")
+    has_next_page: bool = Field(..., description="Has next page")
